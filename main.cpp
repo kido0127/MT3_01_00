c4b9f884afcb7948ae16094442fff8c9da1d9ca2
kido0127 <k024g0012@g.neec.ac.jp>
2025-04-18 11:39:49 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,155 @@
+#include <Novice.h>
+#include <cmath>
+struct Matrix4x4
+{
+	float m[4][4];
+};
+struct Vector3 {
+	float x, y, z;
+};
+const char kWindowTitle[] = "MT3_01_00";
+
+//1.透視投影行列
+/// <summary>
+/// 透視投影行列を作成する
+/// </summary>
+/// <param name="fovY">視野角</param>
+/// <param name="aspectRatio">アスペクト比</param>
+/// <param name="nearClip">近クリップ面</param>
+/// <param name="farClip">遠クリップ面</param>
+/// <returns>透視投影行列</returns>
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 result = {};
+	float tanHalfFovY = std::tanf(fovY / 2.0f);
+
+	result.m[0][0] = 1.0f / (aspectRatio * tanHalfFovY);
+	result.m[1][1] = 1.0f / tanHalfFovY;
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1.0f;
+	result.m[3][2] = -(nearClip * farClip) / (farClip - nearClip);
+
+	return result;
+}
+//2.正射影行列
+/// <summary>
+/// 正射影行列を作成する
+/// </summary>
+/// <param name="left">左</param>
+/// <param name="top">上</param>
+/// <param name="right">右</param>
+/// <param name="bottom">下</param>
+/// <param name="nearClip">近クリップ面</param>
+/// <param name="farClip">遠クリップ面</param>
+/// /// <returns>正射影行列</returns>
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result = {};
+
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 1.0f / (farClip - nearClip);
+	result.m[3][0] = -(right + left) / (right - left);
+	result.m[3][1] = -(top + bottom) / (top - bottom);
+	result.m[3][2] = -nearClip / (farClip - nearClip);
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+//3.ビューポート変換行列
+/// <summary>
+/// ビューポート変換行列を作成する
+/// </summary>
+/// <param name="left">左</param>
+/// <param name="top">上</param>
+/// <param name="width">幅</param>
+/// <param name="height">高さ</param>
+/// <param name="minDepth">最小深度</param>
+/// <param name="maxDepth">最大深度</param>
+/// /// <returns>ビューポート変換行列</returns>
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result = {};
+
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -height / 2.0f;
+	result.m[2][2] = maxDepth - minDepth;
+	result.m[3][0] = left + width / 2.0f;
+	result.m[3][1] = top + height / 2.0f;
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1.0f;
+
+	return result;
+}
+
+
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label) {
+	// 最初にラベルを表示
+	Novice::ScreenPrintf(x, y, "%s", label);
+
+	// 次の行から数値を表示
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 4; j++) {
+			Novice::ScreenPrintf(x + j * kColumnWidth, y + (i + 1) * kRowHeight, "%6.02f", matrix.m[i][j]);
+		}
+	}
+}
+
+
+
+
+// Windowsアプリでのエントリーポイント(main関数)
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	// ライブラリの初期化
+	Novice::Initialize(kWindowTitle, 1280, 720);
+	Matrix4x4 orthograpicMatrix = MakeOrthographicMatrix(-160.0f, 160.0f, 200.0f, 300.0f, 0.0f, 1000.0f);
+	Matrix4x4 perspectiveFovMatrix = MakePerspectiveFovMatrix(0.63f, 1.33f, 0.1f, 1000.0f);
+	Matrix4x4 viewportMatrix = MakeViewportMatrix(100.0f, 200.0f, 600.0f, 300.0f, 0.0f, 1.0f);
+
+	// キー入力結果を受け取る箱
+	char keys[256] = {0};
+	char preKeys[256] = {0};
+
+	// ウィンドウの×ボタンが押されるまでループ
+	while (Novice::ProcessMessage() == 0) {
+		// フレームの開始
+		Novice::BeginFrame();
+
+		// キー入力を受け取る
+		memcpy(preKeys, keys, 256);
+		Novice::GetHitKeyStateAll(keys);
+
+		///
+		/// ↓更新処理ここから
+		///
+
+		///
+		/// ↑更新処理ここまで
+		///
+
+		///
+		/// ↓描画処理ここから
+		///
+
+		MatrixScreenPrintf(0, 0, orthograpicMatrix, "orthographicMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 5, perspectiveFovMatrix, "perspectiveFovMatrix");
+		MatrixScreenPrintf(0, kRowHeight * 10, viewportMatrix, "viewportMatrix");
+
+		///
+		/// ↑描画処理ここまで
+		///
+
+		// フレームの終了
+		Novice::EndFrame();
+
+		// ESCキーが押されたらループを抜ける
+		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+			break;
+		}
+	}
+
+	// ライブラリの終了
+	Novice::Finalize();
+	return 0;
+}
